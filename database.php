33a981fa39225ba443321e115d132d9b3f7c9201
Rakhul <rakhul@outlook.com>
2019-09-10 18:46:32 +1000

    Update database.php

--- database.php
+++ database.php
@@ -1,19 +1,15 @@
+
 <?php
-$mysqli = new mysqli("wildcry.c8vcpeuwbjfj.us-east-2.rds.amazonaws.com","root","rakhulkarthick","wildcry");
+$servername = "wildcry.c8vcpeuwbjfj.us-east-2.rds.amazonaws.com";
+$username = "root";
+$password = "rakhulkarthick";
 
-if ($mysqli->connect_error) {
-  exit('Could not connect');
+// Create connection
+$conn = mysqli_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
-
-$sql = "Select name from tables";
-$stmt = $mysqli->prepare($sql);
-$stmt->bind_param("s", $_GET['q']);
-$stmt->execute();
-$stmt->store_result();
-$stmt->bind_result($name);
-$stmt->fetch();
-$stmt->close();
-
-echo "wildcry";
-echo "<h3>".$name."</h3>";
+echo "Connected successfully";
 ?>
