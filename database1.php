4c1630cc763b54bf3c120b4e50f79d7d57baf138
Rakhul <rakhul@outlook.com>
2019-09-10 21:03:21 +1000

    Update database1.php

--- database1.php
+++ database1.php
@@ -1,6 +1,21 @@
 <?php
 
-    echo "0 results";
+$mysqli = new mysqli("wildcry.c8vcpeuwbjfj.us-east-2.rds.amazonaws.com","root","rakhulkarthick","wildcry");
+if($mysqli->connect_error) {
+  exit('Could not connect');
+}
+
+$sql = "SELECT name FROM tables";
+$stmt = $mysqli->prepare($sql);
+$stmt->bind_param("s", $_GET['q']);
+$stmt->execute();
+$stmt->store_result();
+$stmt->bind_result($name);
+$stmt->fetch();
+$stmt->close();
+
+echo "Fact";
+echo "<h3>".$name."</h3>";
 
 ?>
 
